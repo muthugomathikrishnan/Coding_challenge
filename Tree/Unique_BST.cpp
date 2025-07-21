11a2bb981f745e832e2f9cb1b9a689422ce07db4
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-07-21 21:58:18 +0530

    Create Unique_BST.cpp

---
+++ Tree/Unique_BST.cpp
@@ -0,0 +1,13 @@
+// Link :https://leetcode.com/problems/unique-binary-search-trees/
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n<=1) return 1;
+        vector<int>dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++) dp[i]+=dp[j-1]*dp[i-j];
+        }
+        return dp[n];      
+    }
+};
