2787c499699808bab97a70f4d3c5f0252b448f5c
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-07-23 20:06:21 +0530

    Update climbing_satairs.cpp

--- climbing_satairs.cpp
+++ climbing_satairs.cpp
@@ -15,3 +15,42 @@ public:
 
 // TC -> O(N)
 // SC -> O(N)
+
+
+// Tabulation :
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1) return 1;
+        vector<int>nums(n+1,0);
+        nums[0]=nums[1]=1;
+        for(int i=2;i<=n;i++) nums[i]=nums[i-1]+nums[i-2];
+        return nums[n];
+    }
+};
+
+// TC -> O(N)
+// SC -> O(N)
+
+
+// Optimal Solution:
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1) return 1;
+        int a=1,b=1;
+        int i=3;
+        int c=a+b;
+        while(i<=n){
+            a=b;
+            b=c;
+            c=a+b;
+            i++;
+        }
+        return c;
+    }
+};
+
+// TC -> O(N)
+// SC -> O(N)
