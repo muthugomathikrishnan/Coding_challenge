79c4ae4351c4ebddb24ff32eb2a06b6b0ceb1452
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-07-28 17:56:49 +0530

    Update Swap_BST.cpp

--- Tree/Swap_BST.cpp
+++ Tree/Swap_BST.cpp
@@ -28,3 +28,54 @@ public:
 
 // TC -> O(N)
 // SC -> O(N)
+
+
+// Sol 2 : Morries Traversal
+
+
+class Solution {
+
+public:
+    void recoverTree(TreeNode* root) {
+        if(!root) return ;
+        TreeNode * first=NULL;
+        TreeNode * second=NULL;
+        TreeNode * prev=NULL;
+        TreeNode * curr=root;
+
+        while(curr){
+            if(curr->left){
+                TreeNode * p=curr->left;
+                while(p->right && p->right!=curr) p=p->right;
+
+                if(!p->right){ p->right=curr;curr=curr->left;}
+
+                else {
+                    p->right=NULL;
+                    if(prev && prev->val>curr->val){
+                        if(!first) first=prev;
+                        second=curr;
+                    }
+                    prev=curr;
+                    curr=curr->right;
+                }
+
+            }
+
+            else {
+                if(prev &&  prev->val>curr->val){
+                    if(!first) first=prev;
+                    second=curr;
+                }
+                prev=curr;
+                curr=curr->right;
+            }
+        }
+
+        if(first && second) swap(first->val,second->val);    
+    }
+};
+
+
+// TC -> O(N)
+// SC -> O(1)
