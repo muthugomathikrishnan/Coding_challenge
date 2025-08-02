86cea1361266a91fdceb57a34b04c3e3851cb848
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-02 14:59:41 +0530

    Update Valid_parantheses.cpp

--- Valid_parantheses.cpp
+++ Valid_parantheses.cpp
@@ -27,3 +27,34 @@ public:
 
 // TC -> O(N)
 // SC -> O(N)
+
+
+// Sol 2 : without stack 
+
+class Solution {
+
+private:
+    bool Ismatch(char c1, char c2){
+        if( (c1=='(' && c2==')') ||
+            (c1=='{' && c2=='}') ||
+            (c1=='[' && c2==']')  ) return true;
+        return false;
+    }
+public:
+    bool isValid(string s) {
+     int top=-1;
+     int n=s.length();
+
+     for(int i=0;i<n;i++){
+
+        if(top<0 || !Ismatch(s[top],s[i])){
+            top++;
+            s[top]=s[i];
+        }
+        else top--;
+     }
+     
+    return top==-1;
+        
+    }
+};
