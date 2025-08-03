1b430d6327ac593a8682deffac24fcc4d5b1c443
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-03 12:17:53 +0530

    Update Long_C_prefix.cpp

--- Long_C_prefix.cpp
+++ Long_C_prefix.cpp
@@ -15,3 +15,27 @@ public:
 
 // TC -> O(N Log N)
 // SC -> O(1)
+
+
+// Sol 2 : without sorthing 
+// User function template for C++
+class Solution {
+  public:
+    string longestCommonPrefix(vector<string> arr) {
+        if(arr.empty()) return "";
+        string prefix=arr[0];
+        
+        for(int i=1;i<arr.size();i++){
+            int j=0;
+            while(j<prefix.size() && j<arr[i].size() && prefix[j]==arr[i][j]) j++;
+            prefix=prefix.substr(0,j);
+            if(prefix.empty()) return "";
+        }
+        return prefix;
+        
+    }
+};
+
+
+// TC -> O(N)
+// SC -> O(1)
