cf1bd602013c28f7539f8f130d3b4818f9d35635
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-03 11:54:59 +0530

    Update Binary_to_decimal.cpp

--- Binary_to_decimal.cpp
+++ Binary_to_decimal.cpp
@@ -1,6 +1,19 @@
 // Link :https://www.geeksforgeeks.org/problems/binary-number-to-decimal-number3525/1
 
-// Sol 1:  Using Bitwise Shift
+// Sol 1 : Normal solution 
+class Solution {
+  public:
+    int binaryToDecimal(string &b) {
+        int ans=0,base=1;
+        for(int i=b.length()-1;i>=0;i--){
+            if(b[i]=='1') ans+= base;
+            base*=2;
+        }
+        return ans;
+    }
+};
+
+// Sol 2:  Using Bitwise Shift
 
 class Solution {
   public:
