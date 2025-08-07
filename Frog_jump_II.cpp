0f70f977b9a8acff8f8881fccdce677607fdd8ad
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-07 14:37:03 +0530

    Create Frog_jump_II.cpp

---
+++ Frog_jump_II.cpp
@@ -0,0 +1,12 @@
+// Link 
+class Solution {
+public:
+    int maxJump(vector<int>& stones) {
+     int ans=stones[1];
+     for(int i=2;i<stones.size();i++)
+        ans=max(ans,stones[i]-stones[i-2]);   
+     return ans;
+    }
+};
+// TC -> O(N)
+// sc -> O(1)
