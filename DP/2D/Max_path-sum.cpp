dd8ca130c34b871bad44053dcd65d66f430610ce
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-09 22:46:36 +0530

    Update Max_path-sum.cpp

--- DP/2D/Max_path-sum.cpp
+++ DP/2D/Max_path-sum.cpp
@@ -18,3 +18,26 @@ public:
         return f(n-1,m-1,grid,dp);
     }
 };
+
+// Sol 2: Tabulation
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) dp[0][0]=grid[0][0];
+                else {
+                    int left=INT_MAX,right=INT_MAX;
+                    if(j>0) right=dp[i][j-1]+grid[i][j];
+                    if(i>0) left=dp[i-1][j]+grid[i][j];
+                    dp[i][j]=min(right,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
