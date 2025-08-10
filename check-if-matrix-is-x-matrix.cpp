b209dc036715fdb1b55f5f7e05568023815b9af8
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-10 22:46:25 +0530

    Create check-if-matrix-is-x-matrix.cpp

---
+++ check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,18 @@
+// Link :https://leetcode.com/problems/check-if-matrix-is-x-matrix/submissions/1730375726/
+
+// Sol 
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || i+j==n-1){
+                    if(grid[i][j]==0) return 0;
+                }
+                else if(grid[i][j]!=0 && i!=j) return 0;
+            }
+        }
+        return 1;
+    }
+};
