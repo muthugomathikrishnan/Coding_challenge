c7ad0db94dcf464f8229154097ff71ee35c5b242
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-11 09:56:20 +0530

    Update Minimum_falling_path_sum.cpp

--- DP/2D/Minimum_falling_path_sum.cpp
+++ DP/2D/Minimum_falling_path_sum.cpp
@@ -24,3 +24,51 @@ public:
 };
 
 // Tabulation :
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix[0].size();
+        int m=matrix.size();
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<n;i++) dp[0][i]=matrix[0][i];
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                int dir=(j<n-1) ? dp[i-1][j+1]+ matrix[i][j] : INT_MAX;
+                int dil=(j>0) ? dp[i-1][j-1] + matrix[i][j] : INT_MAX;
+                int up=dp[i-1][j]+matrix[i][j];
+                dp[i][j]=min(up,min(dir,dil));
+                }
+        }
+        int res=dp[m-1][0];
+        for(int j=0;j<n;j++){
+             res=min(res,dp[m-1][j]);
+        }
+        return res;
+    }
+};
+
+
+// Optimal :
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix[0].size();
+        int m=matrix.size();
+        vector<int>prev(n,0),curr(n,0);
+        for(int i=0;i<n;i++) prev[i]=matrix[0][i];
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                int dir=(j<n-1) ? prev[j+1]+ matrix[i][j] : INT_MAX;
+                int dil=(j>0) ? prev[j-1] + matrix[i][j] : INT_MAX;
+                int up=prev[j]+matrix[i][j];
+                curr[j]=min(up,min(dir,dil));
+                }
+                prev=curr;
+        }
+        int res=prev[0];
+        for(int j=0;j<n;j++){
+             res=min(res,prev[j]);
+        }
+        return res;
+    }
+};
