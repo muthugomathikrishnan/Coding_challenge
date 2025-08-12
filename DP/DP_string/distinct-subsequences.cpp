da9e447d0b08d214ab3aa4708e3f56dc7a1a70fd
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-12 18:02:15 +0530

    Update distinct-subsequences.cpp

--- DP/DP_string/distinct-subsequences.cpp
+++ DP/DP_string/distinct-subsequences.cpp
@@ -17,3 +17,21 @@ class Solution {
         return f(i-1,j-1,txt,pat,dp);
     }
 };
+
+// Tabulation :
+class Solution {
+    int p=1e9+7;
+public:
+    int numDistinct(string s, string t) {
+        int n1=s.size(),n2=t.size();
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        for(int i=0;i<=n1;i++) dp[i][0]=1;
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s[i-1]==t[j-1]) dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])% p;
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n1][n2];
+    }
+};
