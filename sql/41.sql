11e749ca0bd62b2d0c78c5fc5289b2d77df925ea
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-15 11:35:46 +0530

    Create 41.sql

---
+++ sql/41.sql
@@ -0,0 +1,11 @@
+-- Link : https://leetcode.com/problems/restaurant-growth/submissions/1735708077/?envType=study-plan-v2&envId=top-sql-50
+with daily_totals as(select visited_on, sum(amount)as amount from Customer group by visited_on )
+
+select a.visited_on,sum(b.amount) as amount,
+round(sum(b.amount)/7.0,2) as average_amount
+from daily_totals a 
+join daily_totals b
+on b.visited_on between a.visited_on -INTERVAL '6 days' and a.visited_on
+group by a.visited_on
+having count(b.visited_on)=7
+order by a.visited_on
