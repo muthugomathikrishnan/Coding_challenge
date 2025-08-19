f7bc62cbc6b6dcb0ebeb0243cb5aaf53dd262a56
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-19 18:07:56 +0530

    Create Population.cpp

---
+++ Tree/Population.cpp
@@ -0,0 +1,37 @@
+// Link : https://leetcode.com/problems/populating-next-right-pointers-in-each-node/submissions/1740817996/
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return root;
+        Node * lm_node=root;
+        while(lm_node->left){
+            Node * curr=lm_node;
+            while(curr){
+                curr->left->next=curr->right;
+                if(curr->next)
+                curr->right->next=curr->next->left;
+                curr=curr->next;
+            }
+            lm_node=lm_node->left;
+        }
+        return root;
+    }
+};
