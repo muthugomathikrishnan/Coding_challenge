4f3f289962eff076a3a5db2e2c57b91326d2eff1
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-20 07:44:32 +0530

    Update 46.sql

--- sql/46.sql
+++ sql/46.sql
@@ -1,5 +1,4 @@
--- link : https://leetcode.com/problems/second-highest-salary/?envType=study-plan-v2&envId=top-sql-50
-
+-- link : https://leetcode.com/problems/delete-duplicate-emails/submissions/1741517094/?envType=study-plan-v2&envId=top-sql-50
 DELETE FROM Person p1
 USING Person p2
 WHERE p1.email = p2.email
