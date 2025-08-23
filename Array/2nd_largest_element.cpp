f19556148eca560a6546ca9e67eb336c21a99d5b
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-23 14:56:06 +0530

    Create 2nd_largest_element.cpp

---
+++ Array/2nd_largest_element.cpp
@@ -0,0 +1,14 @@
+// Link : https://www.naukri.com/code360/problems/second-largest-element-in-the-array_873375?leftPanelTabValue=SUBMISSION
+#include <bits/stdc++.h> 
+int findSecondLargest(int n, vector<int> &arr)
+{
+    int max_1=INT_MIN, max_2=INT_MIN;
+    for(int n: arr){
+        if(n>max_1){
+            max_2=max_1;
+            max_1=n;
+        }
+        else if(n>max_2 && n<max_1) max_2=n;
+    }
+    return (max_2==INT_MIN) ? -1 : max_2;
+}
