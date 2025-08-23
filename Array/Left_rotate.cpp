31bf124c8e829cbd4478fa7d41da4ad44af701ad
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-23 15:39:21 +0530

    Update Left_rotate.cpp

--- Array/Left_rotate.cpp
+++ Array/Left_rotate.cpp
@@ -7,3 +7,16 @@ vector<int> rotateArray(vector<int>& arr, int n) {
 
     return arr;
 }
+
+
+// without reverse:
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) { 
+    n-=1;
+    int s=arr.size(); 
+    n%=s; 
+    vector<int>temp(s,0); 
+    for(int i=0;i<n;i++) temp[i]=arr[s-n+i]; 
+    for(int i=0;i<s-n;i++) temp[n+i]=arr[i];
+    return temp; 
+}
