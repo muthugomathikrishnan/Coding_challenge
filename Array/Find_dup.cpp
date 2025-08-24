ec793121fb32246dfe946be568622945084c4b32
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-24 12:18:56 +0530

    Create Find_dup.cpp

---
+++ Array/Find_dup.cpp
@@ -0,0 +1,14 @@
+// Link : https://www.naukri.com/code360/problems/find-duplicate-in-array_8289592?leftPanelTabValue=SUBMISSION
+
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	int slow=arr[0],fast=arr[0];
+	do{
+		slow=arr[slow];
+		fast=arr[arr[fast]];
+	} while(slow!=fast);
+	fast=arr[0];
+	while(slow!=fast) { slow=arr[slow]; fast=arr[fast];}
+	return slow;
+}
