85a87acc933fd6ba95141d374d3dc2c38256f0b2
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-24 12:17:37 +0530

    Update Rearrange.cpp

--- Array/Rearrange.cpp
+++ Array/Rearrange.cpp
@@ -1,22 +1,19 @@
 // Link : https://leetcode.com/problems/rearrange-array-elements-by-sign/
-
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        int l=nums.size();
+        int l = nums.size();
         
-        vector<int>p(l/2,0),n(l/2,0);
-        int i1=0,j1=0;
-        for(int i: nums) {
-            if(i>0) p[i1++]=i;
-            else n[j1++]=i;
+        vector<int> p, n;
+        for (int i : nums) {
+            if (i > 0) p.push_back(i);
+            else n.push_back(i);
         }
-        int i=0;
-        while(i<l){
-            nums[i++]=p.front();
-            p.erase(p.begin());
-            nums[i++]=n.front();
-            n.erase(n.begin());
+
+        int i1 = 0, j1 = 0;
+        for (int i = 0; i < l; i++) {
+            if (i % 2 == 0) nums[i] = p[i1++];  
+            else nums[i] = n[j1++];            
         }
 
         return nums;
