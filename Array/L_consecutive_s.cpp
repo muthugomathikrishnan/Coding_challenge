ba4ce0d16467c57c1529836cfdc2e6e128ea2da2
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-26 23:03:26 +0530

    Update L_consecutive_s.cpp

--- Array/L_consecutive_s.cpp
+++ Array/L_consecutive_s.cpp
@@ -22,3 +22,32 @@ int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
     }
     return l;
 }
+
+// Sol : 2
+#include <bits/stdc++.h>
+/*bool search(vector<int>&arr ,int x){
+    for(int n:arr)
+        if(n==x) return 1;
+    return 0;
+}
+*/
+
+int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
+    if(n==0) return 0;
+    unordered_set<int>st(arr.begin(),arr.end());
+    int l=1;
+    int last_e=arr[0];
+    int cnt=1;
+    for(int i:arr){
+        if(st.find(i-1)==st.end()){
+            int cnt=1;
+            int x=i;
+            while(st.find(x+1)!=st.end()){
+                x++;
+                cnt++;
+            }
+            l=max(l,cnt);
+        }
+    }
+    return max(l,cnt);
+}
