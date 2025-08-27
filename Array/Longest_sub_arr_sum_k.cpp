9e195dfd1b230f6994f142d019bf2901a0b77d94
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-27 10:17:34 +0530

    Update Longest_sub_arr_sum_k.cpp

--- Array/Longest_sub_arr_sum_k.cpp
+++ Array/Longest_sub_arr_sum_k.cpp
@@ -15,3 +15,22 @@ int longestSubarrayWithSumK(vector<int> a, long long k) {
 }
 
 
+
+// Sol 2:
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int n=a.size();
+    int r=0;
+    long long p_sum=0;
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++){
+        p_sum+=a[i];
+        if(p_sum==k) r=max(r,i+1);
+        long long rem=p_sum-k;
+        if(mp.find(rem)!=mp.end())
+            r=max(r,i-mp[rem]);
+        else if(mp.find(p_sum)==mp.end())    
+            mp[p_sum]=i;
+    }
+    return r;
+}
