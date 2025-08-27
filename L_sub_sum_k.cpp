bcc564466c46c60e4146a5ad41b74bc338a50e6f
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-27 10:00:53 +0530

    Create L_sub_sum_k.cpp

---
+++ L_sub_sum_k.cpp
@@ -0,0 +1,15 @@
+// Link :https://www.naukri.com/code360/problems/longest-subarray-with-sum-k_6682399?leftPanelTabValue=SUBMISSION
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int n=a.size();
+    int r=0;
+
+    for(int i=0;i<n;i++){
+        long long  p_sum=0;
+        for(int j=i;j<n;j++){
+            p_sum+=a[j];
+            if(p_sum==k) r=max(r,j-i+1);
+        }
+    }
+    return r;
+}
