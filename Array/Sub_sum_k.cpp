df0aea337a4f666c52f92f4a24a190e25a76fece
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-08-27 10:55:29 +0530

    Update Sub_sum_k.cpp

--- Array/Sub_sum_k.cpp
+++ Array/Sub_sum_k.cpp
@@ -15,3 +15,23 @@ public:
         return cnt;
     }
 };
+
+// Sol 2: 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int cnt=0;
+        unordered_map<int,int>mp;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum==k) cnt++;
+            int rem=sum-k;
+            if(mp.find(rem)!=mp.end()) cnt+=mp[rem];
+            mp[sum]++;
+            
+        }
+        return cnt;
+    }
+};
