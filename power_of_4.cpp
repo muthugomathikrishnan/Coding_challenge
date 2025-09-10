935b45b2de4b275835250dc1c75cc9af6b2c0d44
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-09-10 21:51:35 +0530

    Create power_of_4.cpp

---
+++ power_of_4.cpp
@@ -0,0 +1,12 @@
+// Link : https://leetcode.com/problems/power-of-four/submissions/1766237416/
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0) return 0;
+        while(n!=1){
+            if(n%4!=0) return 0;
+            n/=4;
+        }
+        return 1;
+    }
+};
