29cc5bbc22c3c132231059cd3d446ceaf966c74b
muthugomathikrishnan <149676883+muthugomathikrishnan@users.noreply.github.com>
2025-09-21 10:43:52 +0530

    Update largest-rectangle-in-a-histogram.cpp

--- Monotonic_stack/largest-rectangle-in-a-histogram.cpp
+++ Monotonic_stack/largest-rectangle-in-a-histogram.cpp
@@ -15,3 +15,26 @@ int largestRectangle(vector < int > & heights) {
   }
   return maxi;
 }
+
+
+// optimal solution:
+#include<bits/stdc++.h>
+int largestRectangle(vector < int > & heights) {
+  int n=heights.size();
+  int maxi=-1e9;
+  stack<int>st;
+  for(int i=0;i<=n;i++){
+    int h=(i==n ? 0:heights[i]);
+    while(!st.empty() && h<heights[st.top()]){
+      int height=heights[st.top()];
+      st.pop();
+      int right=i;
+      int left=st.empty()? -1 : st.top();
+      int width=right-left-1;
+      maxi=max(maxi,height*width);
+
+    }
+    st.push(i);
+  }
+  return maxi;
+}
